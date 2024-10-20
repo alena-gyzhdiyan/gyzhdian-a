ab024fd9a622d9090ede09095c30188c8ed6f8bd
alena.gyzhdiyan <alena@MacBook-Pro-Alena.local>
2024-10-20 18:31:17 +0300

    notification element

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,44 @@
 export default class NotificationMessage {
+    element;
+    timer;
+    isActive
 
+    constructor(message, props = {}) {
+      const {duration = 1000, type = 'success'} = props;
+      this.message = message;
+      this.duration = duration;
+      this.type = type;
+      this.element = this.createElement();
+    }
+
+    createTemplate() {
+      return `
+        <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      <div class="notification-header">${this.type}</div>
+      <div class="notification-body">
+      ${this.message}
+      </div>
+    </div>
+  </div>`;
+    }
+
+    createElement() {
+      const element = document.createElement('div');
+      element.innerHTML = this.createTemplate();
+      return element.firstElementChild;
+    }
+    show(targerElement = document.body) {
+      if (this.isActive) {
+        this.destroy();
+      }
+      this.isActive = this;
+      targerElement.append(this.element);
+      this.timer = setTimeout(()=> this.destroy(), this.duration);
+    }
+
+    remove() {return this.element.remove();}
+
+    destroy() {return this.remove();}
 }
